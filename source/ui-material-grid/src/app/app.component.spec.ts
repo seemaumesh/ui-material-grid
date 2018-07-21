bb9db5126569071c567549184b996bec1100b1e2
sedewan <sedewan@australianunity.com.au>
2018-07-21 14:23:41 +1000

    unit test

--- source/ui-material-grid/src/app/app.component.spec.ts
+++ source/ui-material-grid/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MatToolbarModule,
+  MatCardModule } from '@angular/material';
+  import { ColorPickerModule } from 'ngx-color-picker';
+  import { HttpClientModule } from '@angular/common/http';
+
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [MatToolbarModule,
+      MatCardModule,
+      ColorPickerModule,
+      HttpClientModule]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +23,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it('should contain results and saved properties', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ui-material-grid!');
+    expect(compiled.querySelectorAll('mat-toolbar')[0].textContent.indexOf('Results') >= 0).toBeTruthy();
+    expect(compiled.querySelectorAll('mat-toolbar')[1].textContent.indexOf('Saved Properties') >= 0).toBeTruthy();
   }));
 });
